23ba339bbb4a00e5ad0f4482c3ea0349014738b6
Donald Thompson <don@Donalds-iMac.local>
2020-08-17 06:47:09 -0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Donald Thompson on 8/17/20.
+//  Copyright © 2020 Donald Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
