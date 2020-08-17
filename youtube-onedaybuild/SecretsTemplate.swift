ebaddcce025ed29ea85d87deab79de961806c573
Donald Thompson <don@Donalds-iMac.local>
2020-08-17 07:47:53 -0700

    Added a secrets template to outline how to use the secrets file

---
+++ youtube-onedaybuild/SecretsTemplate.swift
@@ -0,0 +1,20 @@
+//
+//  SecretsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Donald Thompson on 8/17/20.
+//  Copyright © 2020 Donald Thompson. All rights reserved.
+//
+
+import Foundation
+
+/*
+
+ Create a file named Secrets.swift.  Copy this struct to it and add your youtube API key
+ 
+ 
+ struct Secrets {
+    static var API_KEY = ""
+}
+
+ */
